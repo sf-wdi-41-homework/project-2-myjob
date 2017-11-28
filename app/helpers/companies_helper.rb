8407de817a3262c16156bab588bcb573071ee0aa
pengfei <pengfeiye1994@gmail.com>
2017-11-28 12:21:12 -0800

    authentication build to prevent user from going into login contents pages

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -2,4 +2,8 @@ module CompaniesHelper
   def company_params
     params.require(:companies).permit(:progress, :company_name, :postitions_reference, :contact1, :contact2, :contact3)
   end
+
+  def company_params_id
+    @company = Company.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'companies#index', as: 'dashboard'
   get '/new/company', to: 'companies#new', as: 'add_company'
   post '/new/company', to: 'companies#create'
+  get '/company/:id', to: 'companies#show', as: 'company'
   get '/companies/:user_id', to: 'companies#chart'
 
   get '/profile', to: 'profiles#index', as: 'my_profile'
