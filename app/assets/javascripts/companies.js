b52d3880ecac2949f520db48ff4d3c67da9c4f33
pengfei <pengfeiye1994@gmail.com>
2017-11-28 16:18:14 -0800

    Tahl's chart update

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
     console.log("jquery working")
     $('#progress_edit').on('click', function(){
       $('.progress_icon').hide()
-      $('.progress_change').append('<div></div>')
+      $('.progress_change').append('')
     })
 });
