e0b413a660795c42103b5bf50cd6a95a0daf0a93
pengfei <pengfeiye1994@gmail.com>
2017-11-29 23:08:46 -0800

    company progress chart fix position

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -2,7 +2,6 @@
 
 
 $(document).on('turbolinks:load',function(){
-  console.log("test")
   $('.progress_change').on('change', function(e){
       e.preventDefault()
       console.log($(this).attr('id'))
