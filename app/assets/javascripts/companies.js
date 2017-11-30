f76610c9f3b0b4f221c8afa25d5d55759df87f6e
pengfei <pengfeiye1994@gmail.com>
2017-11-29 22:27:16 -0800

    turbolink fix AGAIN, chartkick and groupdate gem implemented

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,12 +1,12 @@
-$( document ).ready(function() {
-    console.log("jquery working")
 
 
-    $('.progress_change').on('change', function(e){
+
+$(document).on('turbolinks:load',function(){
+  console.log("test")
+  $('.progress_change').on('change', function(e){
       e.preventDefault()
       console.log($(this).attr('id'))
       $(this).submit()
-
-    })
-
-  });
+    });
+  }
+)
